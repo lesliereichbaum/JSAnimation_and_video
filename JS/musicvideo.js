aae8c4b15a9317323e643132c27dad9fe73845b8
Leslie <lreichbaum@gmail.com>
2020-07-24 11:21:26 -0400

    first visual studio push includes JS animationa and music video embed

---
+++ JS/musicvideo.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 305) {
+            clearInterval(id);
+        }  else {
+                pos++;
+                elem.style.top = pos + 'px';
+                elem.style.left = pos + 'px';
+            }
+        }
+    }
